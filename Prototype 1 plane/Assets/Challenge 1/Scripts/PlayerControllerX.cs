9c22543fdf903b6c7cb93ef183c8510d5a9993ce
satriafauji <102895019+satriafauji@users.noreply.github.com>
2022-04-06 17:51:45 +0700

    Tugas programming basic

---
+++ Prototype 1 plane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed = 40.0f;
+    public float verticalInput;
+    public float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * forwardInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right, rotationSpeed * verticalInput * Time.deltaTime);
+    }
+}
